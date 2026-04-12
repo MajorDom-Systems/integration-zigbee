2f856c1a7e0dcc0e42ecddac1d878ce5b0da5cfa
Mark Parker <34688010+MarkParker5@users.noreply.github.com>
2026-04-12 18:09:02 +0200

    Merge branch 'develop' into zigbee

--- tests/test_controllers/test_zigbee/conftest.py
+++ tests/test_controllers/test_zigbee/conftest.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
 from contextlib import asynccontextmanager
-from typing import Callable
 from unittest.mock import AsyncMock, Mock, patch
 from uuid import UUID
 
@@ -13,9 +12,9 @@ from httpx_ws.transport import ASGIWebSocketTransport
 from jose import jwt
 
 from majordom_hub.coordinator import Coordinator
+from majordom_hub.providers.paths import Paths
 
-with open('cloud.key', 'r') as f:
-    cloud_key = f.read()
+cloud_key = Paths.data.keys.cloud.read_text()
 
 
 @pytest.fixture(scope="session")
@@ -34,16 +33,16 @@ def credentials_repo_mock_zb():
 @pytest_asyncio.fixture(scope="session")
 async def cloud_service_mock_zb():
     with (
-        patch('majordom_hub.coordinator.CloudService.start', new_callable=AsyncMock),
-        patch('majordom_hub.coordinator.CloudService.fetch_all', new_callable=AsyncMock),
-        patch('majordom_hub.coordinator.CloudService.send_message', new_callable=AsyncMock) as mock,
+        patch("majordom_hub.coordinator.CloudService.start", new_callable=AsyncMock),
+        patch("majordom_hub.coordinator.CloudService.fetch_all", new_callable=AsyncMock),
+        patch("majordom_hub.coordinator.CloudService.send_message", new_callable=AsyncMock) as mock,
     ):
         yield mock
 
 
 @pytest_asyncio.fixture(scope="session")
 async def coordinator(cloud_service_mock_zb, credentials_repo_mock_zb):
-    with patch('majordom_hub.coordinator.ServerService.start', new_callable=AsyncMock):
+    with patch("majordom_hub.coordinator.ServerService.start", new_callable=AsyncMock):
         c = Coordinator(is_virtual_mode=True)
         await c.start(wait_forever=False)
         yield c
@@ -59,12 +58,12 @@ async def async_client(coordinator):
 @pytest.fixture(scope="session")
 def get_user_bearer():
     return lambda id: {
-        'Authorization': 'Bearer ' + jwt.encode({
-            'role': 'access',
-            'user_id': id.hex if isinstance(id, UUID) else id,
-            'is_admin': False,
-            'exp': time.time() + 3600
-        }, cloud_key, algorithm='RS256')
+        "Authorization": "Bearer "
+        + jwt.encode(
+            {"role": "access", "user_id": id.hex if isinstance(id, UUID) else id, "is_admin": False, "exp": time.time() + 3600},
+            cloud_key,
+            algorithm="RS256",
+        )
     }
 
 
@@ -75,4 +74,5 @@ def async_client_ws_connect(coordinator, get_user_bearer):
         async with AsyncClient(transport=ASGIWebSocketTransport(app=coordinator.server_service.app), base_url="ws://testserver") as client:
             async with aconnect_ws("/v1/ws/user", client, headers=get_user_bearer(user_id)) as ws:
                 yield ws
+
     return _connect
